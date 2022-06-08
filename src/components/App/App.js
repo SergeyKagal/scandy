15723484a15da2b34e3b8c66fd311955ea20c82c
SergeyKagal <3461995@gmail.com>
2022-06-08 19:29:26 +0300

    feat: add pathes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import React from 'react';
 import { ProjectCover } from '../ProjectCover/ProjectCover';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PATH } from '../../constants/path';
+import Category from '../Category/Category';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <ProjectCover />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={PATH.MAIN} element={<ProjectCover />} />
+          <Route path={PATH.CATEGORY} element={<Category />} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
--- src/components/ProjectCover/ProjectCover.jsx
+++ src/components/ProjectCover/ProjectCover.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { PATH } from '../../constants/path';
 import './ProjectCover.css';
+import { Link } from 'react-router-dom';
 
 export class ProjectCover extends React.Component {
+  clickHandler = () => {
+    console.log('Click');
+  };
   render() {
     return (
       <>
         <div className="cover__wrapper">
           <h1 className="cover__title">Junior Frontend Test Designs</h1>
+
+          <Link to={PATH.CATEGORY}>
+            {' '}
+            <button onClick={this.clickHandler}>run</button>
+          </Link>
         </div>
       </>
     );
