9d1e0155e53894448d4062e697b238702f0c5866
SergeyKagal <3461995@gmail.com>
2022-06-09 13:44:42 +0300

    fix: header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,28 +5,41 @@ import CartButton from './CartButton';
 import CurrencySelect from './CurrencySelect';
 import './Header.css';
 export default class Header extends React.Component {
+  state = {
+    navList: [
+      {
+        id: 1,
+        title: 'clothes',
+        link: PATH.CLOTHES,
+      },
+      {
+        id: 2,
+        title: 'tech',
+        link: PATH.TECH,
+      },
+      {
+        id: 3,
+        title: 'all',
+        link: PATH.ALL,
+      },
+    ],
+  };
   render() {
     return (
       <header className="header">
         <nav className="header__nav">
           <ul className="header__nav-list">
-            <li className="header__nav-list-item">
-              <Link to={PATH.CLOTHES} className="header__nav-link">
-                <span>clothes</span>
-              </Link>
-            </li>
-            <li className="header__nav-list-item">
-              <Link to={PATH.TECH} className="header__nav-link">
-                <span>tech</span>
-              </Link>
-            </li>
-            <li className="header__nav-list-item">
-              <Link to={PATH.ALL} className="header__nav-link">
-                <span>all</span>
-              </Link>
-            </li>
+            {this.state.navList.map((listItem) => {
+              return (
+                <li key={listItem.id} className="header__nav-list-item">
+                  <Link to={listItem.link} className="header__nav-link">
+                    <span>{listItem.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
-          <Link to={PATH.MAIN} className="header__nav-link">
+          <Link to={PATH.MAIN} className=" a-logo">
             <img src="./images/a-logo.svg" alt="a-logo" />
           </Link>
           <div className="header__nav-controls">
