6b09ed142556dfdae825f25417291e83f81bb376
SergeyKagal <3461995@gmail.com>
2022-06-09 16:15:52 +0300

    feat: categorys title

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from '../Header/Header';
+import ProductCardsWrapper from '../ProductCardsWrapper/ProductCardsWrapper';
+import './Category.css';
 
 export default class Category extends React.Component {
   render() {
     return (
       <div>
         <Header activeTitle={this.props.categoryName} />
-        <h2>{this.props.categoryName}</h2>
+        <main className="category__wrapper">
+          <h2 className="category__title">{this.props.categoryName}</h2>
+          <ProductCardsWrapper></ProductCardsWrapper>
+        </main>
       </div>
     );
   }
---
+++ src/components/ProductCardsWrapper/ProductCardsWrapper.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class ProductCardsWrapper extends Component {
+  render() {
+    return <section>ProductCardsWrapper</section>;
+  }
+}
