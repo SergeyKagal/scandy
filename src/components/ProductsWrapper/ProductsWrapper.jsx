ef72aa9a686b8de61d2b131617ecb19d01387137
SergeyKagal <3461995@gmail.com>
2022-06-12 21:52:13 +0300

    feat: show produts from BE

--- src/components/ProductsWrapper/ProductsWrapper.jsx
+++ src/components/ProductsWrapper/ProductsWrapper.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { currency } from '../../constants/currency';
+
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './ProductsWrapper.css';
@@ -8,72 +8,7 @@ import { getData } from '../../utils/getData';
 import { queries } from '../../constants/queries';
 export default class ProductsWrapper extends Component {
   state = {
-    cardsList: [
-      {
-        id: 1,
-        productName: 'Apollo Running Short',
-        imageUrl: './images/cards/Image1.png',
-        price: {
-          amont: [50, 80],
-        },
-      },
-      {
-        id: 2,
-        productName: 'Jupiter Wayfarer',
-        imageUrl: './images/cards/Image2.png',
-        price: {
-          amont: [75, 110],
-        },
-      },
-      {
-        id: 3,
-        productName: 'Jupiter Wayfarer',
-        imageUrl: './images/cards/Image2.png',
-        price: {
-          amont: [75, 110],
-        },
-      },
-      {
-        id: 4,
-        productName: 'Jupiter Wayfarer',
-        imageUrl: './images/cards/Image2.png',
-        price: {
-          amont: [75, 110],
-        },
-      },
-      {
-        id: 5,
-        productName: 'Jupiter Wayfarer',
-        imageUrl: './images/cards/Image2.png',
-        price: {
-          amont: [75, 110],
-        },
-      },
-      {
-        id: 6,
-        productName: 'Jupiter Wayfarer',
-        imageUrl: './images/cards/Image2.png',
-        price: {
-          amont: [75, 110],
-        },
-      },
-      {
-        id: 7,
-        productName: 'Jupiter Wayfarer',
-        imageUrl: './images/cards/Image2.png',
-        price: {
-          amont: [75, 110],
-        },
-      },
-      {
-        id: 8,
-        productName: 'Jupiter Wayfarer',
-        imageUrl: './images/cards/Image2.png',
-        price: {
-          amont: [75, 110],
-        },
-      },
-    ],
+    cardsList: [],
   };
   getCategoriesData = async () => {
     const { categories } = await getData(queries.categories);
@@ -83,11 +18,12 @@ export default class ProductsWrapper extends Component {
     const { products } = categories.filter(
       (item) => item.name === this.props.categoryName
     )[0];
-    console.log(products);
+    console.log(products[0].prices[0]);
+    this.setState({ cardsList: products });
   };
 
   componentDidMount = () => this.getCategoriesData();
-  componentDidUpdate = () => this.getCategoriesData();
+  // componentDidUpdate = () => this.getCategoriesData();
   render() {
     return (
       <section className="products__wrapper">
@@ -95,20 +31,23 @@ export default class ProductsWrapper extends Component {
           {this.state.cardsList.map((card) => {
             return (
               <li className="product__card" key={card.id}>
+                {card.id}
                 <Link
                   to={PATH.PDP}
                   onClick={() => {
                     this.props.setPdpId(card.id);
                   }}
                 >
-                  <img src={card.imageUrl} alt={card.productName} />
-                  <h3 className="product__title">{card.productName}</h3>
+                  <img src={card.gallery[0]} alt={card.id} />
+                  <h3 className="product__title">{card.name}</h3>
                   <div className="product__price">
                     <span className="product__price-currency">
-                      {currency.currencySign[this.props.currentCurrency]}
+                      {card.prices[this.props.currentCurrency].symbol}
                     </span>
                     <span className="product__price-amont">
-                      {card.price.amont[this.props.currentCurrency].toFixed(2)}
+                      {card.prices[this.props.currentCurrency].amount.toFixed(
+                        2
+                      )}
                     </span>
                   </div>
                 </Link>
