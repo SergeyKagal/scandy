a3102b148da955412520fc4636a0048613ff4a77
SergeyKagal <3461995@gmail.com>
2022-08-10 17:45:43 +0300

    fix: remove PLP-only cart item

--- src/utils/remove-from-cart.js
+++ src/utils/remove-from-cart.js
@@ -3,9 +3,8 @@ import store from '../store';
 export const removeCartItem = (product) => {
   store.cartUpdate(
     store.cart.filter((item) => {
-      return (
-        JSON.stringify(item.product.id) !== JSON.stringify(product.id) &&
-        product.isFromPLP &&
+      return !(
+        JSON.stringify(item.product.id) === JSON.stringify(product.id) &&
         item.product.isFromPLP
       );
     })
