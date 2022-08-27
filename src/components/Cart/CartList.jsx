af4c90af4be7f677d1ef4c904a259076b34bd793
SergeyKagal <3461995@gmail.com>
2022-08-27 14:16:47 +0300

    fix: p12 split render in CartList component

--- src/components/Cart/CartList.jsx
+++ src/components/Cart/CartList.jsx
@@ -1,13 +1,16 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { makeButtonClass } from '../../utils/makeButtonClass';
-import { actionPayload } from '../../constants/action-payload';
-import { plusBtn } from '../ButtonBgr/Plus';
-import { minusBtn } from '../ButtonBgr/Minus';
 import { imageChanger } from '../../utils/image-changer';
-import store from '../../store';
 import { observer } from 'mobx-react';
 import { removeZeroQty } from '../../utils/remove-zero-qty';
+import {
+  renderAttributes,
+  renderImage,
+  renderImageChanger,
+  renderPrice,
+  renderQty,
+  renderTitle,
+} from './CartListRenders';
 
 class CartList extends PureComponent {
   state = {
@@ -32,114 +35,20 @@ class CartList extends PureComponent {
   };
 
   render() {
+    const { cartClass, cartItem } = this.props;
+
     return (
-      <li className={`${this.props.cartClass}__list-item`}>
-        <div className={`${this.props.cartClass}__product`}>
-          <h5
-            className={
-              this.props.cartClass === 'cart'
-                ? 'cart__product-brand'
-                : undefined
-            }
-          >
-            {this.props.cartItem.product.brand}
-          </h5>
-          <h5
-            className={
-              this.props.cartClass === 'cart' ? 'cart__product-name' : undefined
-            }
-          >
-            {this.props.cartItem.product.name}
-          </h5>
-          <div className={`${this.props.cartClass}__product-price`}>
-            {
-              this.props.cartItem.product.prices[store.currentCurrency].currency
-                .symbol
-            }
-            {this.props.cartItem.product.prices[
-              store.currentCurrency
-            ].amount.toFixed(2)}
-          </div>
-          <div className={`${this.props.cartClass}__product-attributes`}>
-            {!!this.props.cartItem.product.attributes.length &&
-              this.props.cartItem.product.attributes.map((attribute) => (
-                <div
-                  key={attribute.id}
-                  className={`${this.props.cartClass}__prop-wrapper`}
-                >
-                  <span>{attribute.name}:</span>
-                  <div
-                    className={
-                      attribute.name === 'Color'
-                        ? `${this.props.cartClass}__prop-color-buttons`
-                        : `${this.props.cartClass}__prop-buttons`
-                    }
-                  >
-                    {attribute.items.map((item) => (
-                      <button
-                        key={item.id}
-                        className={makeButtonClass(
-                          attribute.name,
-                          item.isChecked,
-                          this.props.cartClass
-                        )}
-                        style={
-                          attribute.name === 'Color'
-                            ? { backgroundColor: item.value }
-                            : null
-                        }
-                      >
-                        {item.value}
-                      </button>
-                    ))}
-                  </div>
-                </div>
-              ))}
-          </div>
+      <li className={`${cartClass}__list-item`}>
+        <div className={`${cartClass}__product`}>
+          {renderTitle(cartClass, cartItem)}
+          {renderPrice(cartClass, cartItem)}
+          {renderAttributes(cartClass, cartItem)}
         </div>
-        <div className={`${this.props.cartClass}__product-qty`}>
-          <button
-            onClick={() => {
-              store.cartItemQtyChanger(
-                this.props.cartItem.id,
-                actionPayload.increment
-              );
-            }}
-          >
-            {plusBtn(this.props.cartClass)}
-          </button>
-          <p>{this.props.cartItem.product.qty}</p>
-          <button
-            onClick={() => {
-              store.cartItemQtyChanger(
-                this.props.cartItem.id,
-                actionPayload.decrement
-              );
-            }}
-          >
-            {minusBtn(this.props.cartClass)}
-          </button>
-        </div>
-        <img
-          className={`${this.props.cartClass}__product-image`}
-          src={
-            this.props.cartItem.product.images[this.state.imageNumber].imageUrl
-          }
-          alt="product image"
-        />
-        {this.props.cartClass === 'cart' &&
-          this.props.cartItem.product.images.length > 1 && (
-            <div className="image-changer">
-              <button
-                className="image-changer-left"
-                onClick={() => this.imageButtonHandler(actionPayload.decrement)}
-              ></button>
-              <button
-                className="image-changer-right"
-                onClick={() => this.imageButtonHandler(actionPayload.increment)}
-              ></button>
-            </div>
-          )}
+        {renderQty(cartClass, cartItem)}
+        {renderImage(cartClass, cartItem, this.state.imageNumber)}
+        {cartClass === 'cart' &&
+          cartItem.product.images.length > 1 &&
+          renderImageChanger()}
       </li>
     );
   }
