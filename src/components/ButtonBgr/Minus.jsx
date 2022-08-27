d5b50bd341002702a2ade8765b6ce0d4cb8e5e46
SergeyKagal <3461995@gmail.com>
2022-08-27 12:54:53 +0300

    fix: p11 <svg> to sepatated files

--- src/components/ButtonBgr/Minus.jsx
+++ src/components/ButtonBgr/Minus.jsx
@@ -1,40 +1,8 @@
 import React from 'react';
 
 const minusButton = {
-  bag: (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M8 12H16"
-        stroke="#1D1F22"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <rect x="0.5" y="0.5" width="23" height="23" stroke="#1D1F22" />
-    </svg>
-  ),
-  cart: (
-    <svg
-      width="45"
-      height="45"
-      viewBox="0 0 45 45"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M15 22.5H30"
-        stroke="#1D1F22"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <rect x="0.5" y="0.5" width="44" height="44" stroke="#1D1F22" />
-    </svg>
-  ),
+  bag: <img src="./images/minus24.svg" alt="minus-btn" />,
+  cart: <img src="./images/minus45.svg" alt="minus-btn" />,
 };
 
 export const minusBtn = (cartClass) =>
